d16e41a882435ca989f045f3272b51649dafc54b
david-soto-m <davidsotomarchena@protonmail.com>
2022-08-11 20:56:41 +0200

    refractored mdown based suggestions

--- src/build_suggestions/mdown.rs
+++ src/build_suggestions/mdown.rs
@@ -3,10 +3,9 @@ use markdown_extract;
 use regex::Regex;
 use std::path::PathBuf;
 
-pub fn get_build_suggestions(readme_file: &PathBuf) -> Result<(), SuggestionsError> {
+pub fn get_build_suggestions(readme_file: &PathBuf) -> Result<Vec<Vec<String>>, SuggestionsError> {
     let re = Regex::new(r"((?i)compil|instal|build)").unwrap();
-    let round_one = markdown_extract::extract_from_path(&readme_file, &re)?;
-    Ok(())
+    markdown_extract::extract_from_path(&readme_file, &re).map_err(|e| e.into())
 }
 
 #[cfg(test)]
@@ -15,7 +14,15 @@ mod test {
     use std::path::PathBuf;
     #[test]
     fn tokenize() {
-        super::get_build_suggestions(&PathBuf::from("tests/mdowns/Helix.md")).unwrap();
-        super::get_build_suggestions(&PathBuf::from("tests/mdowns/Shortwave.md")).unwrap();
+        let hx = super::get_build_suggestions(&PathBuf::from("tests/mdowns/Helix.md")).unwrap();
+        assert_eq!(
+            hx[0].len(),
+            48
+        );
+        let swave =
+            super::get_build_suggestions(&PathBuf::from("tests/mdowns/Shortwave.md")).unwrap();
+        assert_eq!(swave.len(), 2);
+        assert_eq!(swave[0].len(), 10);
+        assert_eq!(swave[1].len(), 26);
     }
 }
