525eea685e75678b5e0185211b5d6f4c661c830a
david-soto-m <davidsotomarchena@protonmail.com>
2022-10-03 23:26:35 +0200

    abstracting the function calls form the types

--- src/lib.rs
+++ src/lib.rs
@@ -13,3 +13,54 @@ pub mod dirutils;
 pub mod interaction;
 pub mod package_management;
 pub mod projects;
+use crate::{
+    args::{Cli, Commands},
+    build_suggestions::BuildSuggester,
+    interaction::{MinorInteractions, InstallInteractions},
+    package_management::{
+        PackageManagementCore, PackageManagementExt, PackageManagementInteractive,
+    },
+};
+
+pub fn matcher<T, Q, R>(args: Cli, pm: T, inter: Q)
+where
+T: PackageManagementCore + PackageManagementExt + PackageManagementInteractive,
+Q: InstallInteractions + MinorInteractions,
+R: BuildSuggester,
+{
+    match args.com {
+        Commands::Install { url } => {
+            pm.inter_install::<_, R>(&url, inter)
+                .unwrap_or_else(|e| println!("{e}"));
+        }
+        Commands::Uninstall { package } => {
+            pm.uninstall(&package).unwrap_or_else(|e| println!("{e}"))
+        }
+        Commands::Update { package, force } => pm
+            .inter_update(package, force, inter)
+            .unwrap_or_else(|e| println!("{e}")),
+        Commands::Restore { package } => pm.restore(&package).unwrap_or_else(|e| println!("{e}")),
+        Commands::Reinstall { package } => {
+            pm.reinstall(&package).unwrap_or_else(|e| println!("{e}"))
+        }
+        Commands::Rebuild { package } => pm.rebuild(&package).unwrap_or_else(|e| println!("{e}")),
+        Commands::Rename { old_name, new_name } => {
+            pm.rename(&old_name, &new_name)
+                .unwrap_or_else(|e| println!("{e}"));
+        }
+        Commands::List { package } => {
+            pm.list(package, inter)
+                .unwrap_or_else(|e| println!("{e}"));
+        }
+        Commands::Edit { package } => {
+            pm.edit(&package, inter)
+                .unwrap_or_else(|e| println!("{e}"));
+        }
+        Commands::Cleanup => {
+            pm.cleanup().unwrap_or_else(|e| println!("{e}"));
+        }
+        Commands::Bootstrap => {
+            pm.bootstrap().unwrap_or_else(|e| println!("{e}"));
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,18 @@
 use amisgitpm::{
-    args::{Cli, Commands},
+    matcher,
+    args::Cli,
     build_suggestions::BuildSuggestions,
     interaction::Interactor,
-    package_management::{
-        PackageManagementCore, PackageManagementExt, PackageManagementInteractive, PackageManager,
-    },
+    package_management::PackageManager,
 };
+
 use clap::Parser;
 
 fn main() {
     let args = Cli::parse();
     let pm = PackageManager {};
     let inter = Interactor{};
-    match args.com {
-        Commands::Install { url } => {
-            pm.inter_install::<Interactor, BuildSuggestions>(&url, inter)
-                .unwrap_or_else(|e| println!("{e}"));
-        }
-        Commands::Uninstall { package } => {
-            pm.uninstall(&package).unwrap_or_else(|e| println!("{e}"))
-        }
-        Commands::Update { package, force } => pm
-            .inter_update(package, force, inter)
-            .unwrap_or_else(|e| println!("{e}")),
-        Commands::Restore { package } => pm.restore(&package).unwrap_or_else(|e| println!("{e}")),
-        Commands::Reinstall { package } => {
-            pm.reinstall(&package).unwrap_or_else(|e| println!("{e}"))
-        }
-        Commands::Rebuild { package } => pm.rebuild(&package).unwrap_or_else(|e| println!("{e}")),
-        Commands::Rename { old_name, new_name } => {
-            pm.rename(&old_name, &new_name)
-                .unwrap_or_else(|e| println!("{e}"));
-        }
-        Commands::List { package } => {
-            pm.list(package, inter)
-                .unwrap_or_else(|e| println!("{e}"));
-        }
-        Commands::Edit { package } => {
-            pm.edit(&package, inter)
-                .unwrap_or_else(|e| println!("{e}"));
-        }
-        Commands::Cleanup => {
-            pm.cleanup().unwrap_or_else(|e| println!("{e}"));
-        }
-        Commands::Bootstrap => {
-            pm.bootstrap().unwrap_or_else(|e| println!("{e}"));
-        }
-    }
+    matcher::<_, _, BuildSuggestions>(args, pm, inter);
 }
 
 /*Suggestions */
